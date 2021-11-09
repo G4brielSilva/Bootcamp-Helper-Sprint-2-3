869cb7103d1977e482371ff21470cada97864202
Silva <GabrielSilva@gmail.com>
2021-11-09 18:54:12 -0300

    Primeira Versão Exercício 7

---
+++ app.js
@@ -0,0 +1,41 @@
+"use strict";
+/*Crie uma classe que receba uma lista (númerica ou não) e retorne outra contendo:
+  a) O maior valor;
+  b) O menor valor;
+  c) O valor médio.
+Demonstre essa classe com o paradigma funcional e imperativo */
+class NumberList {
+    avarage(list) {
+        let ct = 0;
+        const sum = list.reduce(function (total, valor) {
+            ct++;
+            return valor + total;
+        }, 0);
+        return sum / ct;
+    }
+    smallerBiggerAvarage(list) {
+        let bigger = -Infinity;
+        let smaller = Infinity;
+        let count = 0;
+        let sum = 0;
+        for (let n of list) {
+            if (n > bigger)
+                bigger = n;
+            if (n < smaller)
+                smaller = n;
+            sum += n;
+            count++;
+        }
+        return [smaller, bigger, sum / count];
+    }
+    smallerBiggerAvarageF(list) {
+        const max = Math.max.apply(null, list);
+        const min = Math.min.apply(null, list);
+        return [min, max, this.avarage(list)];
+    }
+}
+let p1 = new NumberList;
+//Imperativa
+console.log(p1.smallerBiggerAvarage([5, 6, 2, 4, 3]));
+//Funcional
+console.log(p1.smallerBiggerAvarageF([10, 5, 2, 2, 3, 8]));
